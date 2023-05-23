3e1f706379177a48bb07d391bd84fdaf46e0d9dc
Simon Lefort <simon.lefort@gmail.com>
2023-05-23 07:55:51 +0200

    feat(health_check): setup health_check endpoint with 200 / empty response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+        })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
